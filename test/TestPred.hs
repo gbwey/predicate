eb3e5b7628770aadae8cd827014402e3ccbcb97f
grant weyburne <gbwey9@gmail.com>
2019-10-14 23:17:05 -0400

    replace easytest with tasty hunit

--- test/TestPred.hs
+++ test/TestPred.hs
@@ -1,41 +1,44 @@
 {-# OPTIONS -Wall #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module Main where
 import Control.Monad
-import EasyTest
+import Test.Tasty
+import Test.Tasty.HUnit
 import PredState
 import Text.Show.Functions ()
 
 main :: IO ()
-main = run suite
+main = suite
 
-suite :: Test ()
-suite = tests
-  [ scope "peq.fail" $ void $ expectLeft (runPred @() (peq 7) (8::Int))
-  , scope "peq.ok" $ void $ expectRight (runPred @() (peq 7) (7::Int))
-  , scope "palg8.fail1 out of bounds" $ void $ expectLeft (runPred @() alg8 0)
-  , scope "palg8.ok1" $ void $ expectRight (runPred @() alg8 1)
-  , scope "palg8.ok2" $ void $ expectRight (runPred @() alg8 2)
-  , scope "palg8.fail1 except 3" $ void $ expectLeft (runPred @() alg8 3)
-  , scope "palg8.fail2 except 4" $ void $ expectLeft (runPred @() alg8 4)
-  , scope "palg8.ok5" $ void $ expectRight (runPred @() alg8 5)
-  , scope "palg8.fail3 except 6" $ void $ expectLeft (runPred @() alg8 6)
-  , scope "palg8.ok7" $ void $ expectRight (runPred @() alg8 7)
-  , scope "palg8.fail4 out of bounds" $ void $ expectLeft (runPred @() alg8 8)
-  , scope "pexists_range.ok1" $ void $ expectRight (runPred @() (PExists (PRange 4 6) `POr` PNull) ([]::[Int]))
-  , scope "pexists_range.ok2" $ void $ expectRight (runPred @() (PExists (PRange 4 6) `POr` PNull) [3::Int ..7])
-  , scope "pexists_range.fail" $ void $ expectLeft (runPred @() (PExists (PRange 4 6) `POr` PNull) ([7::Int ..10]++[1..3]))
-  , scope "pexists_range.ok3" $ void $ expectRight (runPred @() (PExists (PRange 4 6) `POr` PNull) [6::Int ..9])
-  , scope "ppred_at_index.fail1" $ void $ expectLeft (runPred @() (PIx 3 pfalse (peq 'z')) ("abcdef"::String))
-  , scope "ppred_at_index.fail2" $ void $ expectLeft (runPred @() (PIx 3 pfalse (peq 'z')) (""::String))
-  , scope "ppred_at_index.ok1" $ void $ expectRight (runPred @() (PIx 3 pfalse (peq 'z')) ("abcz"::String))
-  , scope "ppred_at_index.fail3" $ void $ expectLeft (runPred @() (PIx 3 pfalse (peq 'z')) ("zzz_zzzz"::String))
-  , scope "pmixed.ok1" $ void $ expectRight (runPred @() (pge 10 `PAnd` ple 20 `PXor` peq 10 `PAnd` plt 30) (11::Int))
-  , scope "pmixed.fail1" $ void $ expectLeft (runPred @() (pge 10 `PAnd` ple 20 `PXor` peq 10 `PAnd` plt 30) (7::Int))
-  , scope "pnum.ok1" $ void $ expectRight (runPred @() ((peq 20 + peq 22 + pgt 24) * (-peq 25) * (-peq 26)) (22::Int))
-  , scope "pnum.ok2" $ void $ expectRight (runPred @() ((peq 20 + peq 22 + pgt 24) * (-peq 25) * (-peq 26)) (28::Int))
-  , scope "pnum.fail1" $ void $ expectLeft (runPred @() ((peq 20 + peq 22 + pgt 24) * (-peq 25) * (-peq 26)) (25::Int))
+suite :: IO ()
+suite = defaultMain $ testGroup "TestPred"
+  [ testCase "peq.fail" $ expectLeft (runPred @() (peq 7) (8::Int))
+  , testCase "peq.ok" $ expectRight (runPred @() (peq 7) (7::Int))
+  , testCase "palg8.fail1 out of bounds" $ expectLeft (runPred @() alg8 0)
+  , testCase "palg8.ok1" $ expectRight (runPred @() alg8 1)
+  , testCase "palg8.ok2" $ expectRight (runPred @() alg8 2)
+  , testCase "palg8.fail1 except 3" $ expectLeft (runPred @() alg8 3)
+  , testCase "palg8.fail2 except 4" $ expectLeft (runPred @() alg8 4)
+  , testCase "palg8.ok5" $ expectRight (runPred @() alg8 5)
+  , testCase "palg8.fail3 except 6" $ expectLeft (runPred @() alg8 6)
+  , testCase "palg8.ok7" $ expectRight (runPred @() alg8 7)
+  , testCase "palg8.fail4 out of bounds" $ expectLeft (runPred @() alg8 8)
+  , testCase "pexists_range.ok1" $ expectRight (runPred @() (PExists (PRange 4 6) `POr` PNull) ([]::[Int]))
+  , testCase "pexists_range.ok2" $ expectRight (runPred @() (PExists (PRange 4 6) `POr` PNull) [3::Int ..7])
+  , testCase "pexists_range.fail" $ expectLeft (runPred @() (PExists (PRange 4 6) `POr` PNull) ([7::Int ..10]++[1..3]))
+  , testCase "pexists_range.ok3" $ expectRight (runPred @() (PExists (PRange 4 6) `POr` PNull) [6::Int ..9])
+  , testCase "ppred_at_index.fail1" $ expectLeft (runPred @() (PIx 3 pfalse (peq 'z')) ("abcdef"::String))
+  , testCase "ppred_at_index.fail2" $ expectLeft (runPred @() (PIx 3 pfalse (peq 'z')) (""::String))
+  , testCase "ppred_at_index.ok1" $ expectRight (runPred @() (PIx 3 pfalse (peq 'z')) ("abcz"::String))
+  , testCase "ppred_at_index.fail3" $ expectLeft (runPred @() (PIx 3 pfalse (peq 'z')) ("zzz_zzzz"::String))
+  , testCase "pmixed.ok1" $ expectRight (runPred @() (pge 10 `PAnd` ple 20 `PXor` peq 10 `PAnd` plt 30) (11::Int))
+  , testCase "pmixed.fail1" $ expectLeft (runPred @() (pge 10 `PAnd` ple 20 `PXor` peq 10 `PAnd` plt 30) (7::Int))
+  , testCase "pnum.ok1" $ expectRight (runPred @() ((peq 20 + peq 22 + pgt 24) * (-peq 25) * (-peq 26)) (22::Int))
+  , testCase "pnum.ok2" $ expectRight (runPred @() ((peq 20 + peq 22 + pgt 24) * (-peq 25) * (-peq 26)) (28::Int))
+  , testCase "pnum.fail1" $ expectLeft (runPred @() ((peq 20 + peq 22 + pgt 24) * (-peq 25) * (-peq 26)) (25::Int))
   ]
 
 alg1 :: Pred z Int
@@ -52,5 +55,15 @@ alg9 = PLen (PRange 2 7)
 
 alg10 :: Pred z Int
 alg10 = PRange 2 7
+
+expectLeft :: Show b => Either a b -> IO ()
+expectLeft = \case
+  Left _ -> pure ()
+  Right e -> assertFailure $ "expected Left but found Right " ++ show e
+
+expectRight :: Show a => Either a b -> IO ()
+expectRight = \case
+  Right _ -> pure ()
+  Left e -> assertFailure $ "expected Right but found Left " ++ show e
 
 
--- test/TestRegexHelper.hs
+++ test/TestRegexHelper.hs
@@ -1,37 +1,56 @@
 {-# OPTIONS -Wall #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module TestRegexHelper where
 import Control.Monad
-import EasyTest hiding (int, int', double)
+import Test.Tasty
+import Test.Tasty.HUnit
 import RegexHelper
 import Text.Regex.Applicative
+import Data.Either
+import Control.Arrow
 --import Test.QuickCheck
 
 main :: IO ()
-main = run suite
+main = suite
 
-suite :: Test ()
-suite = tests
-  [ scope "stringci.ok1" $ expectEq ("AbC" =~ stringCI "aBc") (Just "AbC")
-  , scope "tokenizespaces.ok1" $ expectEq ("   abc def  g   " =~ tokenizeSpaces) (Just ["abc","def","g"])
-  , scope "tokenizeN.ok1" $ expectEq ("abc_----def_g " =~ tokenizeN (`elem` ['_','-'])) (Just ["abc","def","g "])
-  , scope "oneOf.ok1" $ expectEq ("fred" =~ oneOf ["hello", "bye", "fred",""]) (Just "fred")
-  , scope "oneOf.fail1" $ expectEq ("xxx" =~ oneOf ["hello", "bye", "fred",""]) Nothing
-  , scope "findLongestPrefix.ok1" $ expectEq (findLongestPrefix (intersperseP' (replicateM 4) (sym '.') int) "123.44.111.23.3") (Just ([123,44,111,23,3],""))
-  , scope "findLongestPrefix.ok2" $ expectEq (findLongestPrefix (intersperseP' few (sym '.') int) "123.44.111.23.333") (Just ([123,44,111,23,333],""))
-  , scope "findLongestPrefix.ok3" $ expectEq (findLongestPrefix (intersperseP' some (sym '.') int) "123.44.111.23.333") (Just ([123,44,111,23,333],""))
-  , scope "findLongestPrefix.ok4" $ expectEq (findLongestPrefix (intersperseP' many (sym '.') int) "123.44.111.23.333") (Just ([123,44,111,23,333],""))
-  , scope "int.ok1" $ expectEq ("+123" =~ int) (Just 123)
-  , scope "int.ok2" $ expectEq ("123" =~ int) (Just 123)
-  , scope "int.ok3" $ expectEq ("-123" =~ int) (Just (-123))
-  , scope "double.ok1" $ expectEq ("-0.2345" =~ double) (Just (-0.2345))
-  , scope "double.ok2" $ expectAll' (Just 0) (=~ double) ["0","+0","-0","-00000","0.000"]
-  , scope "double.fail1" $ expectAll' Nothing (=~ double) ["0.",".0","x","-"]
-  , scope "quoted.ok1" $ expectEq ("\"abc\"" =~ quoted) (Just "abc")
-  , scope "quoted.fail1" $ expectAll' Nothing (=~ quoted) ["\"\""," \"aa\"","abc","","\"","\"abc" ]
-  , scope "quoted.ok1" $ expectEq ("\"abc\"" =~ quoted) (Just "abc")
+suite :: IO ()
+suite = defaultMain $ testGroup "TestRegexHelper"
+  [ testCase "stringci.ok1" $ (@?=) ("AbC" =~ stringCI "aBc") (Just "AbC")
+  , testCase "tokenizespaces.ok1" $ (@?=) ("   abc def  g   " =~ tokenizeSpaces) (Just ["abc","def","g"])
+  , testCase "tokenizeN.ok1" $ (@?=) ("abc_----def_g " =~ tokenizeN (`elem` ['_','-'])) (Just ["abc","def","g "])
+  , testCase "oneOf.ok1" $ (@?=) ("fred" =~ oneOf ["hello", "bye", "fred",""]) (Just "fred")
+  , testCase "oneOf.fail1" $ (@?=) ("xxx" =~ oneOf ["hello", "bye", "fred",""]) Nothing
+  , testCase "findLongestPrefix.ok1" $ (@?=) (findLongestPrefix (intersperseP' (replicateM 4) (sym '.') int) "123.44.111.23.3") (Just ([123,44,111,23,3],""))
+  , testCase "findLongestPrefix.ok2" $ (@?=) (findLongestPrefix (intersperseP' few (sym '.') int) "123.44.111.23.333") (Just ([123,44,111,23,333],""))
+  , testCase "findLongestPrefix.ok3" $ (@?=) (findLongestPrefix (intersperseP' some (sym '.') int) "123.44.111.23.333") (Just ([123,44,111,23,333],""))
+  , testCase "findLongestPrefix.ok4" $ (@?=) (findLongestPrefix (intersperseP' many (sym '.') int) "123.44.111.23.333") (Just ([123,44,111,23,333],""))
+  , testCase "int.ok1" $ (@?=) ("+123" =~ int) (Just 123)
+  , testCase "int.ok2" $ (@?=) ("123" =~ int) (Just 123)
+  , testCase "int.ok3" $ (@?=) ("-123" =~ int) (Just (-123))
+  , testCase "double.ok1" $ (@?=) ("-0.2345" =~ double) (Just (-0.2345))
+  , testCase "double.ok2" $ expectAll' (Just 0) (=~ double) ["0","+0","-0","-00000","0.000"]
+  , testCase "double.fail1" $ expectAll' Nothing (=~ double) ["0.",".0","x","-"]
+  , testCase "quoted.ok1" $ (@?=) ("\"abc\"" =~ quoted) (Just "abc")
+  , testCase "quoted.fail1" $ expectAll' Nothing (=~ quoted) ["\"\""," \"aa\"","abc","","\"","\"abc" ]
+  , testCase "quoted.ok1" $ (@?=) ("\"abc\"" =~ quoted) (Just "abc")
   ]
+
+liftMaybe :: Eq a => a -> a -> Either (a,a) ()
+liftMaybe expected actual | expected == actual = Right ()
+                          | otherwise = Left (expected, actual)
+
+expectAll' :: (Show a, Show a1, Eq a, Eq a1) => a1 -> (a -> a1) -> [a] -> IO ()
+expectAll' w p = expectAll (liftMaybe w . p)
+
+expectAll :: (Eq a, Show a, HasCallStack, Show b) => (a -> Either b ()) -> [a] -> IO ()
+expectAll p as = case lefts (map (\a -> left (a,) (p a)) as) of
+                   [] -> pure ()
+                   xs@(_:_) -> assertFailure $ "expected all to succeed but " <> show (length xs) <> " failed " <> show xs
+
+
 {-
 >quickCheck (\(a::Double) -> abs(fromJust (show a =~ double) - a) < 0.000000001)
 +++ OK, passed 100 tests.
